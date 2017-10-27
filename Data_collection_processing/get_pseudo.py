c1cf5b293473dc089d405163ccf3f03931c1aa59
Cait Dreisbach <cnd2y@virginia.edu>
2017-10-27 17:03:16 -0400

    Update get_pseudo.py

--- Data_collection_processing/get_pseudo.py
+++ Data_collection_processing/get_pseudo.py
@@ -3,11 +3,11 @@ This code is used to collect datasets from the ArrayExpress database. We only fo
 one pseudomonas array platform by Affymetirx "A-AFFY-30". It will print the url of the zip
 file of raw data for all experiments measured on this platform. 
 '''
-
+urllib.request and urllib.error
 from urllib.request import urlopen #have to do this with python version 3
 import json
 
-json_uh = urllib2.urlopen("http://www.ebi.ac.uk/arrayexpress/json/v2/files?array=A-AFFY-30")
+json_uh = urlopen("http://www.ebi.ac.uk/arrayexpress/json/v2/files?array=A-AFFY-30")
 data_jsonstr = json_uh.read()
 json_uh.close()
 data = json.loads(data_jsonstr)
